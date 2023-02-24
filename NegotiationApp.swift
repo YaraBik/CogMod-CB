9413d814cd63963705172a550e19cdb447ecf994
YaraBik <61055562+YaraBik@users.noreply.github.com>
2023-02-24 14:51:49 +0100

    initial swift view stuff

---
+++ NegotiationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NegotiationApp.swift
+//  Negotiation
+//
+//  Created by J.N. Bikowski on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct NegotiationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
