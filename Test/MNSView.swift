74343ded42a9b6cb97fbadf25d5b845708070381
YaraBik <bikowski24@gmail.com>
2023-03-17 12:15:26 +0100

    Updated MNS View

--- Test/MNSView.swift
+++ Test/MNSView.swift
@@ -17,59 +17,93 @@ struct MNS_Previews: PreviewProvider {
 struct MNSView: View {
     var body: some View {
         VStack {
-            // clock
+            // Clock
             HStack {
                 Text("⏱")
-                Text("time")
+                Text("time") // TODO: start keeping track of game time here
             }
-            // info view
+            // Info View
             HStack {
-                // model view
                 ZStack {
                     Rectangle()
                         .fill()
                         .foregroundColor(.white)
                     Rectangle()
+                        .stroke(lineWidth: 3)
                     VStack {
                         Text("🤖")
-                        Text("Model")
+                            .font(.largeTitle)
+                        Text("Model score: 0") // TODO: add score value here
+                        Text("Model MNS: ?")
                     }
                 }
-                .padding(10)
-
-                // text field
-                TextField("History of bids", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                ZStack {
+                    Rectangle()
+                        .fill()
+                        .foregroundColor(.white)
+                    Rectangle()
+                        .stroke(lineWidth: 3)
+                    Text("Exchange the MNS")
+                }
             }
+            .padding()
             
-            // Choose MNS view
-            HStack {
-                Button("1") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
-                Button("2") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
-                Button("3") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
-                Button("4") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
-                Button("5") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
-                Button("6") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
-                Button("7") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
-                Button("8") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
-                Button("9") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
+            // Submission field
+            ZStack {
+                Rectangle()
+                    .fill()
+                    .foregroundColor(.white)
+                Rectangle()
+                    .stroke(lineWidth: 3)
+                VStack {
+                    Text("My MNS is:")
+                    HStack {  // TODO: add selection actions
+                        Button("1") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
+                            .frame(width: 20.0, height: 20.0)
+                        Button("2") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
+                            .frame(width: 20.0, height: 20.0)
+                        Button("3") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
+                            .frame(width: 20.0, height: 20.0)
+                        Button("4") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
+                            .frame(width: 20.0, height: 20.0)
+                        Button("5") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
+                            .frame(width: 20.0, height: 20.0)
+                        Button("6") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
+                            .frame(width: 20.0, height: 20.0)
+                        Button("7") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
+                            .frame(width: 20.0, height: 20.0)
+                        Button("8") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
+                            .frame(width: 20.0, height: 20.0)
+                        Button("9") { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/ }
+                            .frame(width: 20.0, height: 20.0)
+                    }
+                }
             }
+            .padding()
             
             // Buttons
-            HStack {
+            HStack {  // TODO: add actions
                 Button("Submit MNS") {
                     /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                 }
+                .padding()
                 Button("Quit Game") {
                     /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                 }
                 .foregroundColor(.red)
+                .padding()
             }
-            
+            .padding(.horizontal)
+
             // Scores
-            HStack {
+            HStack { // TODO: add in these values
                 Text("MNS Value: " + "x")
+                    .padding([.bottom, .trailing], 30.0)
                 Text("Score: " + "x")
+                    .padding([.leading, .bottom], 30.0)
             }
+            .padding()
         }
+        .padding()
     }
 }
